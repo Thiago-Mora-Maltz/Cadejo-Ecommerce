d32c479b8f376a00fd7b721d27e3827e2db9b338
Thiago Mora Maltz <thiagovaldemarm@gmail.com>
2022-10-02 05:24:10 -0300

    desafio firebase II

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import { useContext } from "react"
+import CartFrom from '../CartFrom'
 import {CartContext} from '../context/CartContext'
 import CartDetail from './CartDetail'
+import { useState } from 'react'
 
 function Cart() {
-    const {cart, sumaProductosCart, sumaPrecioCart} = useContext(CartContext)
+    const {cart, sumaProductosCart, sumaPrecioCart, clearCart} = useContext(CartContext)
+    const [ordenId, setOrdenId] = useState("");
+
+    const handleId = (id) => {
+      setOrdenId(id);
+  };
+
+      // const date = new Date()
+      // const fecha = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    
     return (
     <>
       {
         cart.length > 0  ?
-        <ul style={{width: '70%'}}>
-          {cart.map(item => <CartDetail key={item.id} item={item}/> )}
-          <li className="list-group-item" style={{textAlign: 'end'}}>Cantidad productos: {sumaProductosCart()}</li>
-          <li className="list-group-item" style={{textAlign: 'end'}}>Precio total: {sumaPrecioCart()}</li>
-        </ul>
+        <div className='row justify-content-center'>
+          <ul style={{width: '70%'}}>
+            {cart.map(item => <CartDetail key={item.id} item={item}/> )}
+            <li className="list-group-item" style={{textAlign: 'end'}}>Cantidad productos: {sumaProductosCart()}</li>
+            <li className="list-group-item" style={{textAlign: 'end'}}>Precio total: {sumaPrecioCart()}</li>
+          </ul>
+          <CartFrom handleId={handleId} cart={cart} sumaPrecioCart={sumaPrecioCart} clearCart={clearCart}/>
+        </div>
         :
-        <h1>No hay productos agregados 😐</h1>  
+        ordenId ? <h1>{ordenId}</h1> :<h1>No hay productos agregados 😐</h1>  
       }
-      
+       
     </>
   )
 }
---
+++ src/Components/CartFrom.jsx
@@ -0,0 +1,52 @@
+import { serverTimestamp } from 'firebase/firestore'
+import React from 'react'
+import { useState } from 'react'
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+function CartFrom({cart, sumaPrecioCart, clearCart, handleId}) {
+    const [nombre, setNombre] = useState()
+    const [email, setEmail] = useState()
+    const [celular, setCelular] = useState()
+    
+    const sendOrder = (e) => {
+        e.preventDefault()
+        const order = {
+            comprador: {
+                        nombre: nombre,
+                        email: email,
+                        celular: celular
+                    },
+                    items: cart,
+                    total: sumaPrecioCart(),
+                    fecha: serverTimestamp(),
+        }
+        const db = getFirestore();
+        const ordersCollection =  collection(db, "orders");
+        addDoc(ordersCollection, order).then((res) => {
+            handleId(res.id);
+            clearCart();
+        });
+    }
+
+    return (
+    <>
+            <form style={{width: '50%'}} onSubmit={(e)=>sendOrder(e)}>
+                    <div className="mb-3">
+                        <label htmlFor="nombre" className="form-label">Nombre</label>
+                        <input type="text" className="form-control" id="nombre" onChange={(e) => setNombre(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="celular" className="form-label">Celular</label>
+                        <input type="text" className="form-control" id="celular" onChange={(e) => setCelular(e.target.value)}/>
+                    </div>
+                    <button className="btn btn-outline-secondary">Generar orden</button>
+            </form>
+    </>
+  )
+}
+
+export default CartFrom
